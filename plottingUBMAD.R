0e634de787c30112c9a37cad4a391527318c803b
vgupta1 <guptavis@usc.edu>
2019-12-20 15:28:06 -0800

    Adding New Plots for Surface Stuff

---
+++ plottingUBMAD.R
@@ -0,0 +1,124 @@
+library(tidyverse)
+library(ggplot2)
+library(stringr)
+library(tikzDevice)
+library(forcats)
+
+#simple plots for the upper bound theorems
+
+dat = read_tsv("Results/simplePlot__4_1_100.tab")
+
+g <- dat %>% ggplot(aes(D, Bound)) + 
+  geom_line(size=1) +
+  theme_bw(base_size = 6) + 
+  theme(legend.position=c(.235, .72), 
+        legend.title=element_blank(), 
+        legend.text=element_text(size=6)) +
+  xlab("Coefficient of Deviation $D$") +
+  ylab("Bound on $\\mathcal{R}_{PP} / \\mathcal{R}_{SP}$")
+g
+
+##Add some dividers to see where it splits
+delta_l = 0.37542149115328854 # S = 4
+delta_m = 0.5809402158035948  # S = 4
+delta_h = .75
+
+g <- g + geom_vline(xintercept=delta_l, linetype="dotted") + 
+  geom_vline(xintercept=delta_m, linetype="dotted") + 
+  annotate("text", label = "Low", 
+           x = 0.15, y = 3.5, size=1.75) + 
+  annotate("text", label = "Medium", 
+           x = 0.49, y = 3.5, size=1.75) + 
+  annotate("text", label = "High", 
+           x = 0.7, y = 3.5, size=1.75) + 
+  annotate("text", label = "$\\delta_L$", 
+           x = delta_l - .025, y = 1, size=1.75) + 
+  annotate("text", label = "$\\delta_M$", 
+           x = delta_m - .025, y = 1, size=1.75) + 
+  annotate("text", label = "$\\delta_H$", 
+           x = delta_h + .03, y = 1, size=1.75)
+
+tikzDevice::tikz(file = "../../TeX/VoPP/Figures/simplePlot.tex", width=2, height=4/6 * 2 )
+g
+dev.off()
+
+
+#####
+#Similar plot but for inverse
+
+g <- dat %>% ggplot(aes(D, 1/Bound)) + 
+  geom_line(size=1) +
+  theme_bw(base_size = 6) + 
+  theme(legend.position=c(.235, .72), 
+        legend.title=element_blank(), 
+        legend.text=element_text(size=6)) +
+  xlab("Coefficient of Deviation $D$") +
+  ylab("Bound on $\\mathcal{R}_{SP} / \\mathcal{R}_{PP}$")
+g
+
+g<- g + geom_vline(xintercept=delta_l, linetype="dotted") + 
+  geom_vline(xintercept=delta_m, linetype="dotted") + 
+  annotate("text", label = "Low", 
+           x = 0.15, y = .95, size=1.75) + 
+  annotate("text", label = "Medium", 
+           x = 0.49, y = .95, size=1.75) + 
+  annotate("text", label = "High", 
+           x = 0.7, y = .95, size=1.75) + 
+  annotate("text", label = "$\\delta_L$", 
+           x = delta_l - .025, y = .26, size=1.75) + 
+  annotate("text", label = "$\\delta_M$", 
+           x = delta_m - .025, y = .26, size=1.75) + 
+  annotate("text", label = "$\\delta_H$", 
+           x = delta_h + .03, y = .26, size=1.75)
+
+tikzDevice::tikz(file = "../../TeX/VoPP/Figures/singlePriceGuarantee.tex", width=2, height=4/6 * 2 )
+g
+dev.off()
+
+
+###Now the fun begins... Try to do it fo ra contour
+dat = read_tsv("tempContour__2_11_100.tab")
+dat = read_tsv("tempContour__1.1_4_100.tab")
+
+#create a set of custom breaks that includes exp(1) for MHR
+bks <- c(1.25, 1.5, exp(1), 3, 3.5, 5)
+bks <- sort(bks)
+
+g <- ggplot() +
+  stat_contour(data=dat, 
+               aes(D, S, z = Bound, 
+                   linetype= factor(..level.. == exp(1), levels = c(F, T) ), 
+                   color= factor(..level..)),  
+                    breaks=bks) + 
+  theme_bw(base_size = 6) + 
+  theme(legend.title=element_blank(), 
+        legend.text=element_text(size=6)) +
+  scale_linetype_discrete(guide=FALSE) + 
+  xlab("Coefficient of Deviation $D$") +
+  ylab("Scale $S$")
+
+#try to manually label with geom_text
+tmp <- ggplot_build(g)$data[[1]]
+unique(tmp$level)
+filter(tmp, level < 1.01, level > .99) %>% select(x, y, level) %>% filter(y > 9.5)
+
+dat.labels <- tribble(
+  ~x, ~y, ~level,
+  0.88277551, 9, 1.67, 
+  0.8373725, 10, 2.33, 
+  0.2642224, 9, 2.72, 
+  0.3004338, 10, "3.00",
+  0.3729097, 9, 3.67, 
+  0.4308003, 10, 4.33
+)
+ 
+g<- g + geom_text(data=dat.labels, aes(x, y, label=level), 
+              size=1.25)
+
+
+
+tikzDevice::tikz(file = "../../TeX/VoPP/Figures/contourPlot.tex", width=2, height=4/6 * 2 )
+g
+dev.off()
+
+
---
+++ simpleExperiments.jl
@@ -0,0 +1,47 @@
+##Simple Plots of MAD UB bound
+using DelimitedFiles
+include("src.jl")  #load up vopp
+
+function simplePlot(file_out_name; 
+					S=4, M=1, N=100)
+	#Create file_out and header
+	outPath = "$(file_out_name)__$(S)_$(M)_$(N)"
+	f = open("$(outPath).tab", "w")
+	writedlm(f, ["D" "Bound"])
+
+	for  D in range(1e-10, stop=vopp.delta_h(S) - 1e-10, length=N)
+		#add a dumb bound for ease in plotting
+		bound = vopp.vopp_ub_MAD(S, 1., D)	
+		writedlm(f, [D bound])
+	end
+	close(f)
+end
+
+function contour_plot(file_out_name;
+				Smin = 1.1, Smax=4, N=100, TOL = 1e-10)
+	#Create file_out and header
+	outPath = "$(file_out_name)__$(Smin)_$(Smax)_$(N)"
+	f = open("$(outPath).tab", "w")
+	writedlm(f, ["S" "D" "Bound"])
+
+	S_grid = range(Smin, stop=Smax, length=N)
+	D_grid = range(1e-10, stop=1, length=N)
+
+	for S in S_grid
+		for D in D_grid
+			if D > vopp.delta_h(S) - TOL
+				continue
+			end
+			bound = vopp.vopp_ub_MAD(S, 1., D)
+			writedlm(f, [S D bound])
+		end
+		flush(f)
+	end
+	close(f)
+end
+
+if ARGS[1] == "Simple"
+	simplePlot(ARGS[2])
+elseif ARGS[1] == "Contour"
+	contour_plot(ARGS[2])
+end
